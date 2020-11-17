20b7b51b71a5a52c192202862eed1bf0805ff1a3
Kevin Kieffer <kdkieffer@icloud.com>
2020-11-16 22:23:40 -0500

    Updated package min OS versions

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "MapKitSearchView",
+    platforms: [.iOS(.v12), .macOS(.v10_14)],
     products: [
         .library(name: "MapKitSearchView", targets: ["MapKitSearchView"]),
     ],
